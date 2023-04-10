535597b45525432c7b04886c5960ea987b4628c9
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-04-10 10:27:00 +0900

    feat :: globalstyle 지정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./components/router";
+import GlobalStyle from "./common/GlobalStyle";
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle/>
       <Router/>
     </BrowserRouter>
   );
