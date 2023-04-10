4311e804ff7d59389fc72307da2337c71bec2650
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-04-10 15:11:27 +0900

    feat :: 회원가입 기능 구현 완료

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import * as _ from "./style";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { signup } from "../../api";
 
 export const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [emailValid, setEmailValid] = useState(true);
-  const [passwordlValid, setPasswordValid] = useState(true);
+  const [passwordValid, setPasswordValid] = useState(true);
+  const navigate = useNavigate();
 
   const handleEmailChange = (e) => {
     const value = e.target.value;
@@ -20,8 +22,19 @@ export const Signup = () => {
     setPasswordValid(value.length >= 8);
   };
 
-  const handleSubmit = () => {
-    // TODO : 회원가입 처리 로직 구현
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (emailValid && passwordValid) {
+      try {
+        const response = await signup(email, password);
+        console.log('회원가입에 성공하셨습니다.',response.data);
+        navigate("/signin")
+      } catch (error) {
+        console.log('회원가입에 실패하셨습니다.',error);
+        navigate("/")
+      }
+    }
   };
   return (
     <div>
@@ -49,14 +62,14 @@ export const Signup = () => {
           onChange={handlePasswordChange}
           placeholder="비밀번호를 입력해 주세요!"
         />
-        {!passwordlValid ? (
+        {!passwordValid ? (
           <p style={{ color: "red", margin: 0 }}> 8자리 이상 입력하세요.</p>
         ) : (
           <p> 8자리 이상 입력하세요.</p>
         )}
         <button
           data-testid="signup-button"
-          disabled={!emailValid || !passwordlValid}
+          disabled={!emailValid || !passwordValid}
         >
           회원 가입
         </button>
